401187fb708a1132b50829f40ef6af01aa318c8c
coderlisan <coderlisan@gmail.com>
2022-05-14 23:20:34 +0600

    added count up down functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [count, setCount] = useState(0);
 
+	const countUp = () => {
+		setCount(count + 1);
+	};
+
+	const countDown = () => {
+		setCount(count - 1);
+	};
+
+	return (
+		<div className="container">
+			<h1>My Simple counter</h1>
+			<button onClick={countUp}>+</button>
+			<p>{count}</p>
+			<button onClick={countDown}>-</button>
+		</div>
+	);
+}
 export default App;
