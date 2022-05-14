98e115bd6e33a9041a024999e76e35708fa844c5
coderlisan <coderlisan@gmail.com>
2022-05-15 00:24:22 +0600

    add quantity amount and total calculations

--- src/App.js
+++ src/App.js
@@ -3,22 +3,55 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [state, setState] = useState({ quantity: 0, amount: 0 });
 
-	const countUp = () => {
-		setCount(count + 1);
+	const addQuantity = () => {
+		setState({
+			...state,
+			quantity: state.quantity + 1,
+		});
 	};
 
-	const countDown = () => {
-		setCount(count - 1);
+	const removeQuantity = () => {
+		setState({
+			...state,
+			quantity: state.quantity - 1,
+		});
 	};
+
+	const addAmount = () => {
+		setState({
+			...state,
+			amount: state.amount + 1,
+		});
+	};
+
+	const removeAmount = () => {
+		setState({
+			...state,
+			amount: state.amount - 1,
+		});
+	};
+
+	console.log(state);
 
 	return (
 		<div className="container">
-			<h1>My Simple counter</h1>
-			<button onClick={countUp}>+</button>
-			<p>{count}</p>
-			<button onClick={countDown}>-</button>
+			<div className="quantity">
+				<h1>Quantity</h1>
+				<button onClick={addQuantity}>+</button>
+				<p>{state.quantity}</p>
+				<button onClick={removeQuantity}>-</button>
+			</div>
+			<div className="amount">
+				<h1>Amount</h1>
+				<button onClick={addAmount}>+</button>
+				<p>{state.amount}</p>
+				<button onClick={removeAmount}>-</button>
+			</div>
+			<div>
+				<h1>Total Amount: {state.amount * state.quantity}</h1>
+			</div>
 		</div>
 	);
 }
